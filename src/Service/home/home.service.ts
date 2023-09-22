49ab852e5d81abc63b98bc3c367b933c7f595347
svera <svera@anticipa.cl>
2023-09-21 23:16:02 -0300

    casi

--- src/Service/home/home.service.ts
+++ src/Service/home/home.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { EncargadoE } from 'src/Clases/Encargado';
 import { URL_ENDPOINT } from 'src/Config/config';
 
 @Injectable({
@@ -8,75 +9,98 @@ import { URL_ENDPOINT } from 'src/Config/config';
 })
 export class HomeService {
 
-  constructor(private http : HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  getCiudad(IdCiudad:number):Observable<any>{
-    return this.http.get<any>(URL_ENDPOINT+'sede/obtener/'+IdCiudad);
+  getCiudad(IdCiudad: number): Observable<any> {
+    return this.http.get<any>(URL_ENDPOINT + 'sede/obtener/' + IdCiudad);
   }
 
-  getSedes(IdCiudad:number):Observable<any>{
-    return this.http.get<any>(URL_ENDPOINT+'sede/listado/'+IdCiudad);
+  getSedes(IdCiudad: number): Observable<any> {
+    return this.http.get<any>(URL_ENDPOINT + 'sede/listado/' + IdCiudad);
   }
 
-  getAreasDeTrabajo(IdSede:number):Observable<any>{
-    return this.http.get<any>(URL_ENDPOINT+'area/listado/'+IdSede);
+  getAreasDeTrabajo(IdSede: number): Observable<any> {
+    return this.http.get<any>(URL_ENDPOINT + 'area/listado/' + IdSede);
   }
 
-  getAulas(IdAreasDeTrabajo:number):Observable<any>{
-    return this.http.get<any>(URL_ENDPOINT+'aula/listado/'+IdAreasDeTrabajo);
+  getAulas(IdAreasDeTrabajo: number): Observable<any> {
+    return this.http.get<any>(URL_ENDPOINT + 'aula/listado/' + IdAreasDeTrabajo);
   }
 
-  asignarAreasDeTrabajo(IdAreasDeTrabajo:number,IdUsuario:number):Observable<any>{
-    const body = { IdUsuario: IdUsuario};
-    return this.http.put(URL_ENDPOINT+'area/asignar/'+IdAreasDeTrabajo,body);
+  asignarAreasDeTrabajo(IdAreasDeTrabajo: number, IdUsuario: number): Observable<any> {
+    const body = { IdUsuario: IdUsuario };
+    return this.http.put(URL_ENDPOINT + 'area/asignar/' + IdAreasDeTrabajo, body);
   }
 
-  delegarAreasDeTrabajo(IdAreasDeTrabajo:number):Observable<any>{
-    const body = { IdUsuario:null};
-    return this.http.put(URL_ENDPOINT+'area/delegar/'+IdAreasDeTrabajo,body);
+  delegarAreasDeTrabajo(IdAreasDeTrabajo: number): Observable<any> {
+    const body = { IdUsuario: null };
+    return this.http.put(URL_ENDPOINT + 'area/delegar/' + IdAreasDeTrabajo, body);
   }
 
-  eliminarAula(IdAula:number):Observable<any>{
-    const body = { Visible:0};
-    return this.http.put(URL_ENDPOINT+'aula/eliminar/'+IdAula,body);
+  eliminarAula(IdAula: number): Observable<any> {
+    const body = { Visible: 0 };
+    return this.http.put(URL_ENDPOINT + 'aula/eliminar/' + IdAula, body);
   }
 
-  eliminarAreasDeTrabajo(IdArea:number):Observable<any>{
-    const body = { Visible:0};
-    return this.http.put(URL_ENDPOINT+'area/eliminar/'+IdArea,body);
-  }
-  
-  getEncargadosSede(IdSede:number):Observable<any>{
-    return this.http.get<any>(URL_ENDPOINT+'encargado/listado/'+IdSede);
+  eliminarAreasDeTrabajo(IdArea: number): Observable<any> {
+    const body = { Visible: 0 };
+    return this.http.put(URL_ENDPOINT + 'area/eliminar/' + IdArea, body);
   }
 
- editarAula(IdAula:number,NomAula:string,CantidadAlumnos:number):Observable<any>{
-    const body = { NomAula:NomAula, CantidadAlumnos	:CantidadAlumnos};
-    return this.http.put(URL_ENDPOINT+'aula/editar/'+IdAula,body);
+  getEncargadosSede(IdSede: number): Observable<any> {
+    return this.http.get<any>(URL_ENDPOINT + 'encargado/listado/' + IdSede);
   }
 
-  agregarAula(IdArea:number,NomAula:string,CantidadAlumnos:number):Observable<any>{
-    const body = { IdArea:IdArea,NomAula:NomAula, CantidadAlumnos	:CantidadAlumnos};
-    return this.http.post(URL_ENDPOINT+'aula/crear',body);
+  editarAula(IdAula: number, NomAula: string, CantidadAlumnos: number): Observable<any> {
+    const body = { NomAula: NomAula, CantidadAlumnos: CantidadAlumnos };
+    return this.http.put(URL_ENDPOINT + 'aula/editar/' + IdAula, body);
   }
 
-  editarAreaDeTrabajo(NomArea:string,IdArea:number):Observable<any>{
-    const body = { NomArea:NomArea};
-    return this.http.put(URL_ENDPOINT+'area/editar/'+IdArea,body);
+  agregarAula(IdArea: number, NomAula: string, CantidadAlumnos: number): Observable<any> {
+    const body = { IdArea: IdArea, NomAula: NomAula, CantidadAlumnos: CantidadAlumnos };
+    return this.http.post(URL_ENDPOINT + 'aula/crear', body);
   }
 
-  agregarAreaDeTrabajo(IdSede:number,NomArea:string):Observable<any>{
-    const body = { IdSede:IdSede,NomArea:NomArea};
-    return this.http.post(URL_ENDPOINT+'area/crear',body);
+  editarAreaDeTrabajo(NomArea: string, IdArea: number): Observable<any> {
+    const body = { NomArea: NomArea };
+    return this.http.put(URL_ENDPOINT + 'area/editar/' + IdArea, body);
   }
 
-  eliminarEncargado(IdUsuario:number):Observable<any>{
+  agregarAreaDeTrabajo(IdSede: number, NomArea: string): Observable<any> {
+    const body = { IdSede: IdSede, NomArea: NomArea };
+    return this.http.post(URL_ENDPOINT + 'area/crear', body);
+  }
+
+  eliminarEncargado(IdUsuario: number): Observable<any> {
     const body = {};
-    return this.http.put(URL_ENDPOINT+'encargado/eliminar/'+IdUsuario,body);
+    return this.http.put(URL_ENDPOINT + 'encargado/eliminar/' + IdUsuario, body);
   }
 
-  getEncargadoObtener(IdUsuario:number):Observable<any>{
-    return this.http.get(URL_ENDPOINT+'encargado/obtener/'+IdUsuario);
+  getEncargadoObtener(IdUsuario: number): Observable<any> {
+    return this.http.get(URL_ENDPOINT + 'encargado/obtener/' + IdUsuario);
+  }
+
+  agregarEncargado(IdSede: number, Encargado: EncargadoE): Observable<any> {
+    const body = {
+      IdUsuario:Encargado.IdUsuario,
+      NomUsuario:Encargado.NomUsuario,
+      ApeUsuario:Encargado.ApeUsuario,
+      Mail:Encargado.Mail,
+      Contrasenia:Encargado.Contrasenia,
+      IdSede:IdSede,
+    };
+    return this.http.post(URL_ENDPOINT + 'encargado/crear', body);
+  }
+
+  editarEncargado(IdSede: number,Encargado: EncargadoE): Observable<any> {
+    const body = {
+      IdUsuario:Encargado.IdUsuario,
+      NomUsuario:Encargado.NomUsuario,
+      ApeUsuario:Encargado.ApeUsuario,
+      Mail:Encargado.Mail,
+      Contrasenia:Encargado.Contrasenia,
+      IdSede:IdSede,
+    };
+    return this.http.put(URL_ENDPOINT + 'encargado/editar/' + body.IdUsuario, body);
   }
 }
-
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,18 +21,18 @@ export class HomeComponent {
   IdSedeActual!: number;
   AreasDeTrabajo!: AreasDeTrabajo[];
   Encargados!: Encargado[];
-  EncargadosSelecionado!: EncargadoE;
+  EncargadosSelecionado: EncargadoE = { IdUsuario: 0, NomUsuario: "", ApeUsuario: "", Mail: "", Contrasenia: "" };
   AreasDeTrabajoSeleccionado!: number;
-  AulaSeleccionada: Aulas[] =[];
+  AulaSeleccionada: Aulas[] = [];
   nombre: string = '';
   nombreAreaT: string = '';
-  cantidad: number = 0; 
+  cantidad: number = 0;
   tipoModalAula!: boolean;
   tipoModalAreaDeTrabajo!: boolean;
   tipoModalEncargadoAE!: boolean;
   nomAreaDeTrabajoSeleccionada: string = '';
 
-  constructor(private homeService:HomeService) { }
+  constructor(private homeService: HomeService) { }
 
   ngOnInit(): void {
     this.datoLocalStorage = JSON.parse(localStorage.getItem('UsuarioLogueado')!);
@@ -40,16 +40,16 @@ export class HomeComponent {
     this.getSedes(this.datoLocalStorage.IdCiudad);
   }
 
-  getCiudad(IdCiudad:number){
-    this.homeService.getCiudad(IdCiudad).subscribe(  (response) => {
-      this.NomCiudad=response.data[0].NomCiudad;
+  getCiudad(IdCiudad: number) {
+    this.homeService.getCiudad(IdCiudad).subscribe((response) => {
+      this.NomCiudad = response.data[0].NomCiudad;
     });
   }
 
-  getSedes(IdCiudad:number){
-    this.homeService.getSedes(IdCiudad).subscribe(  (response) => {
+  getSedes(IdCiudad: number) {
+    this.homeService.getSedes(IdCiudad).subscribe((response) => {
       console.log(response)
-      this.sedes=response.data;
+      this.sedes = response.data;
       this.NomSedeActual = this.sedes[0].NomSede;
       this.IdSedeActual = this.sedes[0].IdSede;
       this.getAreasDeTrabajo(this.IdSedeActual);
@@ -57,120 +57,120 @@ export class HomeComponent {
     });
   }
 
-  getAreasDeTrabajo(IdSede:number){
-    this.homeService.getAreasDeTrabajo(IdSede).subscribe(  (response) => {
-      this.AreasDeTrabajo=response.data;
+  getAreasDeTrabajo(IdSede: number) {
+    this.homeService.getAreasDeTrabajo(IdSede).subscribe((response) => {
+      this.AreasDeTrabajo = response.data;
       this.getAulas();
     });
   }
 
-  getAulas(){
+  getAulas() {
     this.AreasDeTrabajo.forEach(AreaT => {
-      this.homeService.getAulas(AreaT.IdArea).subscribe(  (response) => {
-        AreaT.Aulas=response.data;
+      this.homeService.getAulas(AreaT.IdArea).subscribe((response) => {
+        AreaT.Aulas = response.data;
       });
     });
   }
 
-  CambiarSede(NomSede:string,IdSede:number){
+  CambiarSede(NomSede: string, IdSede: number) {
     this.NomSedeActual = NomSede;
     this.IdSedeActual = IdSede;
     this.getAreasDeTrabajo(IdSede);
     this.getEncargadosSede(IdSede);
   }
-  
 
-  asignarAreasDeTrabajo(IdAreasDeTrabajo:number,IdUsuario:number){
-    this.homeService.asignarAreasDeTrabajo(IdAreasDeTrabajo,IdUsuario).subscribe(  (response) => {
+
+  asignarAreasDeTrabajo(IdAreasDeTrabajo: number, IdUsuario: number) {
+    this.homeService.asignarAreasDeTrabajo(IdAreasDeTrabajo, IdUsuario).subscribe((response) => {
       this.getAreasDeTrabajo(this.IdSedeActual);
     });
   }
 
-  delegarAreasDeTrabajo(IdAreasDeTrabajo:number){
-    this.homeService.delegarAreasDeTrabajo(IdAreasDeTrabajo).subscribe(  (response) => {
+  delegarAreasDeTrabajo(IdAreasDeTrabajo: number) {
+    this.homeService.delegarAreasDeTrabajo(IdAreasDeTrabajo).subscribe((response) => {
       this.getAreasDeTrabajo(this.IdSedeActual);
     });
   }
 
-  eliminarAula(IdAula:number){
-    this.homeService.eliminarAula(IdAula).subscribe(  (response) => {
+  eliminarAula(IdAula: number) {
+    this.homeService.eliminarAula(IdAula).subscribe((response) => {
       this.getAreasDeTrabajo(this.IdSedeActual);
     });
   }
 
-  eliminarAreasDeTrabajo(IdArea:number){
-    this.homeService.eliminarAreasDeTrabajo(IdArea).subscribe(  (response) => {
+  eliminarAreasDeTrabajo(IdArea: number) {
+    this.homeService.eliminarAreasDeTrabajo(IdArea).subscribe((response) => {
       this.getAreasDeTrabajo(this.IdSedeActual);
     });
   }
 
-  Asignar(IdUsuario:any) {
-    this.asignarAreasDeTrabajo(this.AreasDeTrabajoSeleccionado,Number(IdUsuario));
+  Asignar(IdUsuario: any) {
+    this.asignarAreasDeTrabajo(this.AreasDeTrabajoSeleccionado, Number(IdUsuario));
     this.cerrarModalAsignar();
   }
 
-  getEncargadosSede(IdSede:number){
-    this.homeService.getEncargadosSede(IdSede).subscribe(  (response) => {
-      this.Encargados=response.data;
+  getEncargadosSede(IdSede: number) {
+    this.homeService.getEncargadosSede(IdSede).subscribe((response) => {
+      this.Encargados = response.data;
     });
   }
 
-  abrirModalAsignar(IdAreasDeTrabajo: number){
-    this.AreasDeTrabajoSeleccionado=IdAreasDeTrabajo;
+  abrirModalAsignar(IdAreasDeTrabajo: number) {
+    this.AreasDeTrabajoSeleccionado = IdAreasDeTrabajo;
     const modal = document.getElementById('ModalAsignar');
-    if(modal != null){
+    if (modal != null) {
       modal.style.display = 'block';
     }
   }
 
   cerrarModalAsignar() {
     const modal = document.getElementById('ModalAsignar');
-    if(modal != null){
+    if (modal != null) {
       modal.style.display = 'none';
     }
   }
 
-  abrirModalAula(Aula:any,tipoModal:boolean,IdArea:number){ 
-    if(tipoModal){
+  abrirModalAula(Aula: any, tipoModal: boolean, IdArea: number) {
+    if (tipoModal) {
       this.AulaSeleccionada.push(Aula);
-    }else{
-      this.AreasDeTrabajoSeleccionado=IdArea;
-      let Aaula: Aulas = {IdAula:0,NomAula:"",CantidadAlumnos:0,Visible:1,IdArea:0 };
+    } else {
+      this.AreasDeTrabajoSeleccionado = IdArea;
+      let Aaula: Aulas = { IdAula: 0, NomAula: "", CantidadAlumnos: 0, Visible: 1, IdArea: 0 };
       this.AulaSeleccionada.push(Aaula);
     }
-    this.tipoModalAula=tipoModal;
+    this.tipoModalAula = tipoModal;
     const modal = document.getElementById('ModalAula');
-    if(modal != null){
+    if (modal != null) {
       modal.style.display = 'block';
     }
   }
 
   cerrarModalAula() {
     const modal = document.getElementById('ModalAula');
-    if(modal != null){
+    if (modal != null) {
       modal.style.display = 'none';
     }
-    this.AulaSeleccionada=[];
-    this.nombre='';
-    this.cantidad=0;
+    this.AulaSeleccionada = [];
+    this.nombre = '';
+    this.cantidad = 0;
   }
 
   EditarOAgregarAula() {
-    if(this.nombre!='' || this.cantidad!=0){
-      let nnombre =this.AulaSeleccionada[0].NomAula; 
-      let ccantidad =this.AulaSeleccionada[0].CantidadAlumnos; 
-      if(this.nombre!=''){
-        nnombre =this.nombre;
+    if (this.nombre != '' || this.cantidad != 0) {
+      let nnombre = this.AulaSeleccionada[0].NomAula;
+      let ccantidad = this.AulaSeleccionada[0].CantidadAlumnos;
+      if (this.nombre != '') {
+        nnombre = this.nombre;
       }
-      if(this.cantidad!=0){
-        ccantidad =this.cantidad;
+      if (this.cantidad != 0) {
+        ccantidad = this.cantidad;
       }
-      if(this.tipoModalAula){
-        this.homeService.editarAula(this.AulaSeleccionada[0].IdAula,nnombre,ccantidad).subscribe(  (response) => {
+      if (this.tipoModalAula) {
+        this.homeService.editarAula(this.AulaSeleccionada[0].IdAula, nnombre, ccantidad).subscribe((response) => {
           this.getAreasDeTrabajo(this.IdSedeActual);
         });
-      }else{
-        this.homeService.agregarAula(this.AreasDeTrabajoSeleccionado,nnombre,ccantidad).subscribe(  (response) => {
+      } else {
+        this.homeService.agregarAula(this.AreasDeTrabajoSeleccionado, nnombre, ccantidad).subscribe((response) => {
           this.getAreasDeTrabajo(this.IdSedeActual);
         });
       }
@@ -178,37 +178,37 @@ export class HomeComponent {
     this.cerrarModalAula();
   }
 
-  abrirModalEncargado(){ 
+  abrirModalEncargado() {
     const modal = document.getElementById('ModalEncargado');
-    if(modal != null){
+    if (modal != null) {
       modal.style.display = 'block';
     }
   }
   cerrarModalEncargado() {
     const modal = document.getElementById('ModalEncargado');
-    if(modal != null){
+    if (modal != null) {
       modal.style.display = 'none';
     }
   }
 
-  abrirModalAreaDeTrabajo(AreaDeTrabajo:boolean, nomAreaDeTrabajo:string,idAreaDeTrabajo:number){ 
-    this.tipoModalAreaDeTrabajo = AreaDeTrabajo; 
+  abrirModalAreaDeTrabajo(AreaDeTrabajo: boolean, nomAreaDeTrabajo: string, idAreaDeTrabajo: number) {
+    this.tipoModalAreaDeTrabajo = AreaDeTrabajo;
     this.nomAreaDeTrabajoSeleccionada = nomAreaDeTrabajo;
-    this.AreasDeTrabajoSeleccionado =idAreaDeTrabajo;
+    this.AreasDeTrabajoSeleccionado = idAreaDeTrabajo;
     const modal = document.getElementById('ModalAreaDeTrabajo');
-    if(modal != null){
+    if (modal != null) {
       modal.style.display = 'block';
     }
   }
 
   EditarOAgregarAreaDeTrabajo() {
-    if(this.nombreAreaT!= ''){
-      if(this.tipoModalAreaDeTrabajo){
-        this.homeService.agregarAreaDeTrabajo(this.IdSedeActual,this.nombreAreaT).subscribe(  (response) => {
+    if (this.nombreAreaT != '') {
+      if (this.tipoModalAreaDeTrabajo) {
+        this.homeService.agregarAreaDeTrabajo(this.IdSedeActual, this.nombreAreaT).subscribe((response) => {
           this.getAreasDeTrabajo(this.IdSedeActual);
         });
-      }else{
-        this.homeService.editarAreaDeTrabajo(this.nombreAreaT,this.AreasDeTrabajoSeleccionado).subscribe(  (response) => {
+      } else {
+        this.homeService.editarAreaDeTrabajo(this.nombreAreaT, this.AreasDeTrabajoSeleccionado).subscribe((response) => {
           this.getAreasDeTrabajo(this.IdSedeActual);
         });
       }
@@ -218,47 +218,74 @@ export class HomeComponent {
 
   cerrarModalAreaDeTrabajo() {
     const modal = document.getElementById('ModalAreaDeTrabajo');
-    if(modal != null){
+    if (modal != null) {
       modal.style.display = 'none';
-    } 
-    this.nomAreaDeTrabajoSeleccionada = '';
-    this.AreasDeTrabajoSeleccionado =0;
-  }
-
-  abrirModalEncargadoAE(AreaDeTrabajo:boolean,idEncargado:number){ 
-    this.tipoModalEncargadoAE = AreaDeTrabajo; 
-    if(!this.tipoModalEncargadoAE){
-      this.homeService.getEncargadoObtener(idEncargado).subscribe(  (response) => {
-        this.EncargadosSelecionado = response.data;
-        console.log(response.data);
-        const modal = document.getElementById('ModalEncargadoAE');
-        if(modal != null){
-          modal.style.display = 'block';
-        }
-      });
     }
+    this.nomAreaDeTrabajoSeleccionada = '';
+    this.AreasDeTrabajoSeleccionado = 0;
   }
-  cerrarModalEncargadoAE() {
+
+  abrirModalEncargadoAE(AreaDeTrabajo: boolean, idEncargado: number) {
+    this.tipoModalEncargadoAE = AreaDeTrabajo;
+    this.homeService.getEncargadoObtener(idEncargado).subscribe((response) => {
+      this.EncargadosSelecionado = response.data[0];
+      console.log(response.data);
+      const modal = document.getElementById('ModalEncargadoAE');
+      if (modal != null) {
+        modal.style.display = 'block';
+      }
+    });
+  }
+  cerrarModalEncargadoAE(accion:boolean) {
+    if(accion){
+      let  Encargado = this.EncargadosSelecionado;
+      if(this.tipoModalEncargadoAE){
+        if( this.EncargadosSelecionado.NomUsuario!= "" && this.EncargadosSelecionado.ApeUsuario!= "" && this.EncargadosSelecionado.Mail!= "" && this.EncargadosSelecionado.Contrasenia!="" ){
+          this.homeService.agregarEncargado(this.IdSedeActual,Encargado).subscribe((response) => {
+            console.log(response);
+            this.getEncargadosSede(this.IdSedeActual);
+          });
+        }
+      }else{
+        this.homeService.editarEncargado(this.IdSedeActual,Encargado).subscribe((response) => {
+          console.log(response);
+          this.getEncargadosSede(this.IdSedeActual);
+        });
+      }
+    }
     const modal = document.getElementById('ModalEncargadoAE');
-    if(modal != null){
+    if (modal != null) {
       modal.style.display = 'none';
-    } 
+    }
+    this.EncargadosSelecionado = { IdUsuario: 0, NomUsuario: "", ApeUsuario: "", Mail: "", Contrasenia: "" };
   }
 
-
-
-
-
-  eliminarEncargado(IdUsuario:number){
-    this.homeService.eliminarEncargado(IdUsuario).subscribe(  (response) => {
+  eliminarEncargado(IdUsuario: number) {
+    this.homeService.eliminarEncargado(IdUsuario).subscribe((response) => {
       this.getAreasDeTrabajo(this.IdSedeActual);
       this.getEncargadosSede(this.IdSedeActual);
     });
   }
 
+  EncargadoInputChange(tipo: number, event: any) {
+    switch (tipo) {
+      case 1:
+        this.EncargadosSelecionado.NomUsuario = event.target.value;
+        break;
+      case 2:
+        this.EncargadosSelecionado.ApeUsuario = event.target.value;
+        break;
+      case 3:
+        this.EncargadosSelecionado.Mail = event.target.value;
+        break;
+      case 4:
+        this.EncargadosSelecionado.Contrasenia = event.target.value;
+        break;
+    }
+  }
 
   nomAreaDeTrabajoInputChange(event: any) {
-    this.nombreAreaT=event.target.value;
+    this.nombreAreaT = event.target.value;
   }
 
   nombreInputChange(event: any) {
@@ -266,6 +293,6 @@ export class HomeComponent {
   }
 
   edadInputChange(event: any) {
-    this.cantidad = parseInt(event.target.value, 10); 
+    this.cantidad = parseInt(event.target.value, 10);
   }
 }
