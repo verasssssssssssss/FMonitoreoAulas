762ddd39532fd1258f74d69ea556fa2a8a0e5965
Sebastian Vera <sebastian.vera1901@alumnos.ubiobio.cl>
2023-12-06 22:45:48 -0300

    config

--- src/Config/config.ts
+++ src/Config/config.ts
@@ -1,2 +1,3 @@
-export const URL_ENDPOINT = 'http://localhost:3000/';
+//export const URL_ENDPOINT = 'http://localhost:3000/';
 //export const URL_ENDPOINT = 'https://easy-pear-goose-fez.cyclic.cloud/';
+export const URL_ENDPOINT = 'http://pacheco.chillan.ubiobio.cl:3026/';
